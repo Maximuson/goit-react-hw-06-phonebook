326f3a9e7d9feeb2adf66037eab6171f96fb327f
Maxim Pavlenko <maxim@MacBook-Air.local>
2020-04-17 17:44:18 +0300

    Updates

---
+++ precache-manifest.fa7b8bacf74badc69af6b05b4a2ad68e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dae60528e218d5e5d225626a1779aefc",
+    "url": "/Maximuson/goit-react-hw-06-phonebook/index.html"
+  },
+  {
+    "revision": "266b5c676b1bc27e4a5a",
+    "url": "/Maximuson/goit-react-hw-06-phonebook/static/css/main.edfdf93e.chunk.css"
+  },
+  {
+    "revision": "12f71f07707ad9cacc5b",
+    "url": "/Maximuson/goit-react-hw-06-phonebook/static/js/2.a8cdc442.chunk.js"
+  },
+  {
+    "revision": "4d6ab1395afd48c38ab429e9c82a1cee",
+    "url": "/Maximuson/goit-react-hw-06-phonebook/static/js/2.a8cdc442.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "266b5c676b1bc27e4a5a",
+    "url": "/Maximuson/goit-react-hw-06-phonebook/static/js/main.824583e0.chunk.js"
+  },
+  {
+    "revision": "4869aed65cbec3c58204",
+    "url": "/Maximuson/goit-react-hw-06-phonebook/static/js/runtime-main.96007288.js"
+  }
+]);
